d3d8e2d79a3d1e89a06c4471e89941cc4ef79268
UserShri98 <sjc3098@gmail.com>
2025-06-05 11:25:07 +0530

    api use

--- netgpt/src/components/Browse.jsx
+++ netgpt/src/components/Browse.jsx
@@ -1,7 +1,11 @@
 import Header from "./Header";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 
 
 const Browse=()=>{
+
+   useNowPlayingMovies()
+
     return(
         <div>
          <Header/>
--- netgpt/src/components/Header.jsx
+++ netgpt/src/components/Header.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { addUser, removeUser } from "../utils/userSlice";
+import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
     const dispatch=useDispatch();
@@ -42,7 +43,7 @@ const Header = () => {
       {/* LEFT: Netflix Logo */}
       <img
         className="w-40"
-        src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        src={LOGO_URL}
         alt="logo"
       />
 

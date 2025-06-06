d0c87952a82f0ae6d67de2f9d5a04a65ba7aa74a
UserShri98 <sjc3098@gmail.com>
2025-06-06 12:01:21 +0530

    added different movie genre lists

--- netgpt/src/components/MovieList.jsx
+++ netgpt/src/components/MovieList.jsx
@@ -3,12 +3,12 @@ import MovieCard from "./MovieCard";
 const MovieList=({title,movies})=>{
     console.log(movies)
     return (
-        <div className="px-6 bg-black"> 
+        <div className="px-6 "> 
                 <h1 className="text-3xl text-white py-4">{title}</h1>
 
      <div className=" flex overflow-x-scroll">
         <div className="flex">
-            {movies.map(movie=><MovieCard posterPath={movie.poster_path} key={movie.id} />)}
+            {movies?.length > 0 && movies.map(movie=><MovieCard posterPath={movie.poster_path} key={movie.id} />)}
  
         </div>
     </div>

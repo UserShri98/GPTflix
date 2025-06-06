f0de750ad616328c0e026346f716393282e6e0b7
UserShri98 <sjc3098@gmail.com>
2025-06-06 22:15:42 +0530

    language

--- netgpt/src/components/GptSearchBar.jsx
+++ netgpt/src/components/GptSearchBar.jsx
@@ -1,3 +1,4 @@
+import lang from "../utils/languageConstants";
 
 
 
@@ -8,9 +9,9 @@ const GptSearchBar=()=>{
              <input
              type="text"
              className="p-4 m-4 col-span-9 text-white"
-             placeholder="What would you like to watch today?"
+             placeholder={lang.hindi.gptSearchPlaceholder}
              />
-             <button className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg">Search</button>
+             <button className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg">{lang.hindi.search}</button>
             </form>
         </div>
     )

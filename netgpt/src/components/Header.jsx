d62e6b575b44462ff43ad8313942648740ad7e71
UserShri98 <sjc3098@gmail.com>
2025-06-06 13:15:06 +0530

    GPT Search button

--- netgpt/src/components/Header.jsx
+++ netgpt/src/components/Header.jsx
@@ -50,20 +50,22 @@ const Header = () => {
       {/* RIGHT: User profile + Sign Out button */}
       {user && (
         <div className="flex items-center">
+          <button className="text-white p-2 py-2 px-4 mx-4 my-2 bg-purple-800 rounded-lg">GPT Search</button>
+           <button
+            onClick={handleSignOut}
+            className="text-white bg-red-700 px-4 py-2 rounded-md"
+          >
+            Sign Out
+          </button>
           <img
             src={
               user.photoURL ||
               "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"
             }
             alt="user"
-            className="w-10 h-10 rounded-full mr-4"
+            className="w-10 h-10 rounded-full mr-4 p-2"
           />
-          <button
-            onClick={handleSignOut}
-            className="text-white bg-red-700 px-4 py-2 rounded-md"
-          >
-            Sign Out
-          </button>
+         
         </div>
       )}
     </div>
